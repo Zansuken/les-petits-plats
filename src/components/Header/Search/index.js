f8277242e7daa83ab3508048fa5fae6613373b51
Zansuken <doom.sebastien.pro@gmail.com>
2023-09-30 19:01:32 +0200

    Updated Search

--- src/components/Header/Search/index.js
+++ src/components/Header/Search/index.js
@@ -1,17 +1,27 @@
-import { build } from "../../../componentBuilder";
+import { build, updateView } from "../../../componentBuilder";
+import { store } from "../../../store";
 import Button from "../../shared/Button";
 import Icon from "../../shared/Icon";
 import TextField from "../../shared/TextField";
 
+const styles = {
+  root: "px-60 py-36 flex flex-col gap-8 items-center",
+  title: "font-title text-yellow text-4.5xl/[66px] text-center max-w-[720px]",
+};
+
 const Search = () => {
+  const searchInput = () => store.getSearchInput();
+  const onSearch = () => searchInput() && updateView(() => searchInput());
+
+  const { root, title } = styles;
+
   return build(
     "div",
-    { class: "px-60 py-36 flex flex-col gap-8 items-center" },
+    { class: root },
     build(
       "h1",
       {
-        class:
-          "font-title text-yellow text-4.5xl/[66px] text-center max-w-[720px]",
+        class: title,
       },
       "CHERCHEZ PARMI PLUS DE 1500 RECETTES DU QUOTIDIEN,SIMPLES ET DÉLICIEUSES"
     ),
@@ -21,8 +31,13 @@ const Search = () => {
         label: Icon({
           src: "/assets/images/searchIcon.svg",
         }),
+        onclick: onSearch,
       }),
       placeHolder: "Rechercher une recette, un ingrédient, ...",
+      onInput: (event) => {
+        store.setSearchInput(event.target.value ? event.target.value : "");
+      },
+      onKeyEnter: onSearch,
     })
   );
 };
