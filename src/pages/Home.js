a1635661916c33e1d635f62e338d2e139771810c
Doom Sébastien <88394869+Zansuken@users.noreply.github.com>
2023-11-17 15:43:18 +0100

    FIxed post demo found bugs

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,17 +5,24 @@ import Tags from "../components/Home/Tags";
 import {
   displayedRecipesSelector,
   recipesSelector,
+  searchSelector,
   selectedTagsSelector,
   useSelector,
 } from "../store/selectors";
 import { getDisplayedRecipes } from "../helpers/dataHelpers";
+import Typography from "../components/shared/Typography";
+import { classNamesBuilder } from "../helpers/classNames";
 
 const styles = {
-  root: "py-5 px-24 w-full flex-1 bg-bg-grey",
+  root: "py-5 px-24 w-full flex-1 bg-bg-grey flex flex-col",
+  messageHidden: "hidden",
+  messageVisible:
+    "flex w-full h-full justify-center items-center pt-10 text-lg",
 };
 
 const Home = () => {
   const selectedTags = useSelector(selectedTagsSelector);
+  const currentSearch = () => useSelector(searchSelector);
 
   const allSelectedTags = [
     ...selectedTags.ingredients,
@@ -43,6 +50,26 @@ const Home = () => {
     },
     Filters({ recipesCount: currentRecipes.length }),
     Tags({ tags: allSelectedTags }),
+    build(
+      {
+        element: "div",
+        className: classNamesBuilder([
+          {
+            className: styles.messageVisible,
+            isUsed: currentRecipes.length === 0,
+          },
+          {
+            className: styles.messageHidden,
+            isUsed: currentRecipes.length > 0,
+          },
+        ]),
+      },
+      Typography({
+        variant: "h2",
+        value: `Aucune recette ne contient ‘${currentSearch()}’ vous pouvez chercher «
+    tarte aux pommes », « poisson », etc.`,
+      })
+    ),
     HomeContent({ currentRecipes })
   );
 };
